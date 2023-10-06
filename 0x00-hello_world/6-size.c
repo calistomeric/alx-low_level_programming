e070728993176e489599b9a56411a9ba4da836ea
calistus <okonkwocalistus98@gmail.com>
2023-10-06 14:36:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - The main prints the size of various c variables
+ * main - A main program to print the size of various c variables
  * Return: 0 (Success) 
  */
 int main(void)
@@ -10,10 +10,11 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-printf("Size of a char is %d bytes(s)\n", sizeof(a));
-printf("Size of an int is %d bytes(s)\n", sizeof(b));
-printf("Size of a long int is %d bytes(s)\n", sizeof(c));
-printf("Size of a long long int is %d bytes(s)\n", sizeof(d));
-printf("Size of a float is %d byte(s)\n", sizeof(6));
+
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(6));
 return (0);
 }
