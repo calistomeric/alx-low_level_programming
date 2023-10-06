683f2e9afe6904eb4523812ba464fa73b2721ca9
calistus <okonkwocalistus98@gmail.com>
2023-10-06 07:17:10 -0700

    3-name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/** 
+ * main - a function to print a line using put
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
