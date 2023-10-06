7019273077953276e6561a03485c6008294e6603
calistus <okonkwocalistus98@gmail.com>
2023-10-06 07:53:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/** 
- * main - a function to print a line using put
- * Return: 0 (succes)
- */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
+
