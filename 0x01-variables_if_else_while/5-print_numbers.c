f16dab4e72350c517d12d8d241dff137c1b7c6f1
calistus <okonkwocalistus98@gmail.com>
2023-10-07 06:19:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program to print the base ten of single digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	for (i = 0; i <= 9; i++)
+{
+	printf("%d", i);
+	i++;
+}
+	printf("\n");
+	return (0);
+}
