b74e678e805038b73df0f1bee68de5cdc6683f80
calistus <okonkwocalistus98@gmail.com>
2023-10-07 06:59:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A program to print alphabets in small letters reveresed
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
