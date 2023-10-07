d93da8398f5057fc0ed81b076e109358a4109270
calistus <okonkwocalistus98@gmail.com>
2023-10-07 10:42:08 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - A program that prints number combination
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	int b;
+
+	int c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (!((a == b) || ( b == c) || (b > c) || (a > b)))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+				if (!(a == '7' && b == '8' && c == '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
