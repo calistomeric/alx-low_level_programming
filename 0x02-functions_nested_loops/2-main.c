4c66f8a42b2459d9f377a032467ac5c494473be4
calistus <okonkwocalistus98@gmail.com>
2023-10-11 05:21:23 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints lower case alphabets ten times
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case 10 times
+ *
+ * Return: Returns void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j < 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
