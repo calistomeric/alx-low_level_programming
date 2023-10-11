1d8877036794048677fae18e3c6b83063775fdf4
calistus <okonkwocalistus98@gmail.com>
2023-10-11 16:34:57 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - a program that adds two numbers
+ * @a: first input integer
+ * @b: second input integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - code goes here
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
