bc45a4373a79534868786cfa774cf026ea8d3b4a
calistus <okonkwocalistus98@gmail.com>
2023-10-11 08:18:54 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ * _abs - calculates the absolute value of a number
+ * @x: accepts integer as an input
+ * Return: 0
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-1 * x);
+}
