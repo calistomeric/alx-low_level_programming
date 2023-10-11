a308732fcb6a275f634eb401fa105bc47ac15fc1
calistus <okonkwocalistus98@gmail.com>
2023-10-11 08:38:05 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - A program to print the last digit f a number
+ * @x: accepts the parameter as integer
+ * Return: 0
+ */
+
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
