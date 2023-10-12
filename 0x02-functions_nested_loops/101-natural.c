42faa036057ba84184339e1b72f7b3be6f837b92
calistus <okonkwocalistus98@gmail.com>
2023-10-11 18:06:44 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program for natural number
+ * Return: o
+ */
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n",sum);
+	return (0);
+}
