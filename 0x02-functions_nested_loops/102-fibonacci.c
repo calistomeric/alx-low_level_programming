7d90ac175ee80bf05b86aa26e078d04a1e2e84ca
calistus <okonkwocalistus98@gmail.com>
2023-10-11 18:17:24 -0700

    fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		print("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			print("\n");
+		else
+			print(',');
+
+	}
+	return (0);
+}
