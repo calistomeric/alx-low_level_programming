2cefbb3ca97daa4ccdb50a97d46f9847eb6c76ad
calistus <okonkwocalistus98@gmail.com>
2023-10-11 17:57:40 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - The code goes here
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return(0);
+}
