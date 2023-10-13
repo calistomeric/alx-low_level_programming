b47b37fab86704dac1ea9c7eaaa4d185954e5647
calistus <okonkwocalistus98@gmail.com>
2023-10-13 14:15:15 -0700

    debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(month, day, year);
+int convert_day(int month, int day)
+
+#endif
