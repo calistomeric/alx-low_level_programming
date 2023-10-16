ba56ff721d466a49213473fc529ca673f37920cb
calistus <okonkwocalistus98@gmail.com>
2023-10-16 14:57:02 -0700

    put char

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - puts a character on the screen
+ * @str: input string
+ * Return: 0 for success
+ */
+
+void _puts(char *str)
+{
+	int pos = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*(str + pos));
+	}
+	_putchar('\n');
+}
