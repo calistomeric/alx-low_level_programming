712240cf31036a2f99d5022c716d55ffc8912411
calistus <okonkwocalistus98@gmail.com>
2023-10-24 13:18:36 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchar - finds occurence of a character
+ * @s: string to search
+ * @c: search character
+ * Return: pointer to first occurence or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
