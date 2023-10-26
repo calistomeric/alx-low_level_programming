005108c2f47e838ebb6249fb732649b972d12a2d
calistus <okonkwocalistus98@gmail.com>
2023-10-26 15:21:50 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: input string
+ * Return: 0 for success
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
