bcef843991b4c32f8b797ac3ff7b55ea6ee333e6
calistus <okonkwocalistus98@gmail.com>
2023-10-26 15:27:51 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - recursion for string len
+ * @s: string input
+ * Return: 0 for success
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
