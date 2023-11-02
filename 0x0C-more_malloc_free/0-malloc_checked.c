d6f8f9745005bc7e800e34c318af264530dcceee
calistus <okonkwocalistus98@gmail.com>
2023-11-02 04:12:25 -0700

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * malloc_checked - checks malloc
+ * @b: unsigned integer input
+ * Return: 0 for success
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _calloc - calloc operation
+ * @nmemb: input
+ * @size: input
+ * Return: 0 for success
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size = 0)
+		return (NULL);
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (0);
+}
